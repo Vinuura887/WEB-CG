5cbbbad0e238b98cc0d847f1fa1ddd62343fd6ec
Vinuura887 <161715345+Vinuura887@users.noreply.github.com>
2024-06-11 11:51:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-/* function SendMail() {
-    var params = {
-        from_name : document.getElementById("fullName").value,
-        email_id : document.getElementById("email_id").value,
-        message : document.getElementById("message").value
-    }
-    emailjs.send("service_ygfnegn", "template_oyis65a", params).then(function (res) {
-        alert("Enviado com succeso! " + res.status);
-    })
-} */
-
-var params = document.getElementById("input#button")
-params.addEventListener ("click", SendMail)
 function SendMail() {
-    alert("Enviado com succeso!")
+    var params = {
+        from_name: document.getElementById("fullName").value,
+        email_id: document.getElementById("email_id").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_ygfnegn", "template_oyis65a", params)
+        .then(function (res) {
+            alert("Enviado com sucesso! " + res.status);
+        }, function (error) {
+            alert("Erro ao enviar o e-mail: " + JSON.stringify(error));
+        });
+
+    // Retorna false para evitar o envio do formulário padrão
+    return false;
 }
